a88acc9823f3cef694a4b87f03db43af7d0aa63d
Shochan1986 <shotarobanben1986@gmail.com>
2021-11-18 10:51:33 +0900

    cache removed

--- farm/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class FarmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'farm'
-    verbose_name = '農園'
-    verbose_name_plural = '農園'
