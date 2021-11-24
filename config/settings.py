cc9e9f63ea95d1fd8d3d1e6287d17b0794a685b5
Shochan1986 <shotarobanben1986@gmail.com>
2021-11-24 14:47:33 +0900

    cors

--- config/settings.py
+++ config/settings.py
@@ -184,6 +184,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
     'https://nashiya-reports.netlify.app',
+    'https://nashiya-django.herokuapp.com',
 ]
 
 CLOUDINARY_STORAGE = { 
