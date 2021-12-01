417a9632052c3e00023cc2ce8d2534a1962cff12
Shochan1986 <68531220+Shochan1986@users.noreply.github.com>
2021-12-01 10:26:50 +0900

    Update urls.py

--- farm/urls.py
+++ farm/urls.py
@@ -33,7 +33,7 @@ from farm.views.article_views import (
 
 from farm.views.line_views import callback
 
-from farm.views.others_view import createArticleComment
+from farm.views.others_view import createArticleComment, deleteComment
 
 urlpatterns = [
     path('users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -51,6 +51,7 @@ urlpatterns = [
     path('articles/delete/<str:pk>/', deleteArticle),
 
     path('articles/<str:pk>/comments/', createArticleComment, name="create-comment"),
+    path('comments/delete/<str:pk>/', deleteComment),
 
     path('images/create/', createImage),
     path('images/', getImages),
