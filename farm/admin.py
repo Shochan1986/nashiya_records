27f348ed3610b69915c632a8fb3675c69f7c2e2a
Shochan1986 <68531220+Shochan1986@users.noreply.github.com>
2021-12-09 15:37:01 +0900

    Update admin.py

--- farm/admin.py
+++ farm/admin.py
@@ -44,6 +44,10 @@ class ArticleAdmin(admin.ModelAdmin):
         obj.from_admin_site = True 
         obj.save()
         super(ArticleAdmin, self).save_model(request, obj, form, change)
+        
+    def delete_model(self, request, obj):
+        obj.from_admin_site = True
+        super(ArticleAdmin, self).delete_model(request, obj)
 
 
 class ImagesAdmin(admin.ModelAdmin):
