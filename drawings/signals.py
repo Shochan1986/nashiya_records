92b25f5432e39343d3d798e60a71929a0731780c
Shochan1986 <shotarobanben1986@gmail.com>
2021-12-30 21:05:34 +0900

    secure https cloudinary

--- drawings/signals.py
+++ drawings/signals.py
@@ -6,12 +6,12 @@ from drawings.models import Drawing
 @receiver(post_save, sender=Drawing)
 def url_one_save(sender, instance, created, **kwargs):
     if instance.image_one and not instance.url_one:
-        instance.url_one = instance.image_one.url
+        instance.url_one = instance.image_one.build_url(secure=True)
         instance.save()
 
 
 @receiver(post_save, sender=Drawing)
 def url_two_save(sender, instance, created, **kwargs):
     if instance.image_two and not instance.url_two:
-        instance.url_two = instance.image_two.url
+        instance.url_two = instance.image_two.build_url(secure=True)
         instance.save()
