09d4335a050acfabfb986a37bc2c95de0911afb6
Shochan1986 <shotarobanben1986@gmail.com>
2022-01-01 15:59:09 +0900

    serializers

--- drawings/serializers.py
+++ drawings/serializers.py
@@ -5,6 +5,17 @@ from drawings.models import Drawing
 
 
 class DrawingSerializer(serializers.ModelSerializer):
+    image_one = serializers.SerializerMethodField(read_only=True)
+    image_two = serializers.SerializerMethodField(read_only=True)
+
+    def get_image_one(self, obj):
+        return obj.image_one.build_url(secure=True)
+
+    def get_image_two(self, obj):
+        if obj.image_two:
+            return obj.image_two.build_url(secure=True)
+        else:
+            return None
 
     class Meta:
         model = Drawing
