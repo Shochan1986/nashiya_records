1381add57db76b3f578355e7afe4c1c88d747403
Shochan1986 <shotarobanben1986@gmail.com>
2022-01-01 16:07:10 +0900

    secure

--- config/settings.py
+++ config/settings.py
@@ -210,7 +210,6 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env("CLOUDINARY_CLOUD_NAME"),
     'API_KEY': env("CLOUDINARY_API_KEY"),
     'API_SECRET': env("CLOUDINARY_API_SECRET"),
-    'SECURE': True,
 }   
 
 if os.getcwd() == '/app':
