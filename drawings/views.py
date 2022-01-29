21be8c4409022b6eefd74932fa3c05ff9634ed55
Shochan1986 <shotarobanben1986@gmail.com>
2022-01-30 07:43:37 +0900

    permission

--- drawings/views.py
+++ drawings/views.py
@@ -71,7 +71,7 @@ def createDrawingComment(request, pk):
 
 
 @api_view(['GET'])
-#@permission_classes([IsAdminUser])
+@permission_classes([IsAdminUser])
 def getComment(request, pk):
     comment = Comment.objects.get(id=pk)
     serializer = CommentSerializer(comment, many=False)
