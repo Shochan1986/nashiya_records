1a61569f2c67def05a0ee29348d30a27dc704ac0
Shochan1986 <shotarobanben1986@gmail.com>
2022-03-27 22:31:06 +0900

    content image

--- photos/models.py
+++ photos/models.py
@@ -121,7 +121,6 @@ class ContentImage(models.Model):
     content_image = CloudinaryField(
         blank=True, 
         null=True, 
-        secure=True,
         transformation={
             "quality": "auto:best", 
             'dpr': "auto", 
