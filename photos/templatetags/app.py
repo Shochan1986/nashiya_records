7cfe5e052f32436096bfdf53b2e51ef70dc5bc7b
Shochan1986 <shotarobanben1986@gmail.com>
2022-03-28 19:31:23 +0900

    app deleted

--- photos/templatetags/app.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.utils.safestring import mark_safe
-from markdownx.utils import markdownify
-
-register = template.Library()
-
-@register.filter
-def markdown_to_html(text):
-    return mark_safe(markdownify(text))
