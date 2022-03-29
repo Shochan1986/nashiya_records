414e72fb6e6549d1c43781f121fefa06a5a3be66
Shochan1986 <shotarobanben1986@gmail.com>
2022-03-29 13:53:24 +0900

    urls

--- config/urls.py
+++ config/urls.py
@@ -15,17 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url, include as inc
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('farm.urls')),
     path('pics/', include('drawings.urls')),
     path('photos/', include('photos.urls')),
-    url(r'mdeditor/', inc('mdeditor.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
