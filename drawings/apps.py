4f4e834056192ec4a2014e7f26a6ecb2112704be
Shochan1986 <shotarobanben1986@gmail.com>
2022-04-27 07:34:42 +0900

    kousaku

--- drawings/apps.py
+++ drawings/apps.py
@@ -4,8 +4,8 @@ from django.apps import AppConfig
 class DrawingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'drawings'
-    verbose_name = '絵画'
-    verbose_name_plural = '絵画'
+    verbose_name = '絵画・工作'
+    verbose_name_plural = '絵画・工作'
 
     def ready(self):
         import drawings.signals
