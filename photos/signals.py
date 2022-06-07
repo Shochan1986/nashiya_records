1a801f0c48fda9b4b6571ae9730b0891f5340c06
Shochan1986 <shotarobanben1986@gmail.com>
2022-06-07 16:13:10 +0900

    reply message

--- photos/signals.py
+++ photos/signals.py
@@ -8,13 +8,9 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.models import (
     TextSendMessage, 
 )
-from requests import request
 from farm.models import LinePush
-from photos.models import Comment, AlbumLike, Metadata
+from photos.models import Comment, AlbumLike, Reply
 from environs import Env 
-import metadata_parser
-from rest_framework.response import Response
-from rest_framework import status
 
 env = Env() 
 env.read_env()  
@@ -44,6 +40,43 @@ def comment_create_notification(sender, instance, created, **kwargs):
             }
             subject =  f'コメント＠{instance.image.title}'
             message = render_to_string('photos/comment_message.txt', context)
+            from_email = settings.DEFAULT_FROM_EMAIL
+            bcc = []
+            for user in User.objects.filter(is_staff=True):
+                bcc.append(user.email)
+            email = EmailMessage(subject, message, from_email, [], bcc)
+            email.send()
+
+
+@receiver(post_save, sender=Reply)
+def reply_create_notification(sender, instance, created, **kwargs):
+    try:
+        if created:
+            comment = instance.comment
+            writer = instance.comment.author
+            album = comment.image
+            context = {
+                'album' : album,
+                'writer' : writer,
+                'replier' : instance.author,
+                'text' : instance.text,
+            }
+            message = render_to_string('photos/reply_message.txt', context)
+            for push in LinePush.objects.filter(unfollow=False):
+                line_bot_api.push_message(push.line_id, messages=TextSendMessage(text=message))
+    except:
+        if created:
+            comment = instance.comment
+            writer = instance.comment.author
+            album = comment.image
+            context = {
+                'album' : album,
+                'writer' : writer,
+                'replier' : instance.author,
+                'text' : instance.text,
+            }
+            subject =  f'「{instance.author}」さんから「{writer}」さんへの返信　@アルバム「{album}」'
+            message = render_to_string('photos/reply_message.txt', context)
             from_email = settings.DEFAULT_FROM_EMAIL
             bcc = []
             for user in User.objects.filter(is_staff=True):
